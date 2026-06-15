e90f613273bbb7b50131a1dfdd13c9dd87786cf2
ElizabethBrooks <merimaat1@gmail.com>
2026-06-15 11:06:05 -0400

    Working on fixes for package installation

--- functional_profiler/app.R
+++ functional_profiler/app.R
@@ -3,6 +3,7 @@
 # Modified: 15 June 2026
 
 # install any missing packages
+options(repos = c(CRAN = "https://cloud.r-project.org/"))
 packageList <- c("shiny", "ggplot2", "vegan", "devtools")
 biocList <- c("edgeR", "ALDEx2", "DESeq2", "limma", "phyloseq")
 devList <- c("cafferychen777/ggpicrust2")
